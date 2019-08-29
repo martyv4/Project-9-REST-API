f1846855f2a06c87a96101fc11e2f220f5db68b6
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-08-28 22:45:28 -0400

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
-const Sequelize = require('sequelize');
+
 
 const app = require('./routes/routes.js');
 
@@ -13,46 +13,7 @@ const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'tr
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
-// TODO setup your api routes here
 
-// setup a friendly greeting for the root route
-app.get('/', (req, res) => {
-  const sql = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'fsjstd-restapi.db'
-  });
-  
-  const test = sql.authenticate()
-  .then(function () {
-      console.log("CONNECTED! ");
-  })
-  .catch(function (err) {
-      console.log("FAILED");
-  })
-  .done(); 
-  res.json({
-    message: 'Welcome to the REST API project!',
-  });
-});
-
-// send 404 if no other route matched
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route Not Found',
-  });
-});
-
-// setup a global error handler
-app.use((err, req, res, next) => {
-  if (enableGlobalErrorLogging) {
-    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
-  }
-
-  res.status(err.status || 500).json({
-    message: err.message,
-    error: {},
-  });
-});
 
 // set our port
 app.set('port', process.env.PORT || 5000);
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,45 @@
 const express = require('express');
 const app = express();
 
+const Sequelize = require('sequelize');
 
+// setup a friendly greeting for the root route
+app.get('/', (req, res) => {
+    const sql = new Sequelize({
+      dialect: 'sqlite',
+      storage: 'fsjstd-restapi.db'
+    });
 
+const test = sql.authenticate()
+.then(function () {
+    console.log("CONNECTED! ");
+})
+.catch(function (err) {
+    console.log("FAILED");
+})
+.done(); 
+res.json({
+  message: 'Welcome to the REST API project!',
+});
+});
 
+// send 404 if no other route matched
+app.use((req, res) => {
+res.status(404).json({
+  message: 'Route Not Found',
+});
+});
+
+// setup a global error handler
+app.use((err, req, res, next) => {
+if (enableGlobalErrorLogging) {
+  console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+}
+
+res.status(err.status || 500).json({
+  message: err.message,
+  error: {},
+});
+});
 
 module.exports = app;
